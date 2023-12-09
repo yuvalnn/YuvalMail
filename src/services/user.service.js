714b03c98a1b790612a64cbaa57773bf6fd4c58a
YuvalN <yuvalno@gmail.com>
2023-12-09 05:27:13 +0200

    Add mock user service for testing
    This commit introduces a mock user service, user.service.js, specifically created for testing purposes in our React application. The service currently contains hard-coded user properties and is utilized to simulate user-related functionality during the development and testing phase.
    
    - New file: user.service.js
    - Mocks user data for testing
    - No actual login system implemented

---
+++ src/services/user.service.js
@@ -0,0 +1,16 @@
+
+export const robotService = {
+    GetloggedinUser,
+
+}
+
+
+
+const loggedinUser = { email: 'user@appsus.com', fullname: 'Mahatma Appsus' }
+
+
+function GetloggedinUser()
+{
+    return loggedinUser    
+}
+
